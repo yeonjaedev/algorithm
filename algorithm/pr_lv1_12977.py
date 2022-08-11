ef8a385082174982496915905226858f3f855dfc
yeonjaedev <yjae47@gmail.com>
2022-08-11 23:47:46 +0900

    [UPDATE] 12977

--- algorithm/pr_lv1_12977.py
+++ algorithm/pr_lv1_12977.py
@@ -4,6 +4,11 @@
 from itertools import combinations
 def solution(nums):
     answer = 0
+    # for i in range(len(nums)-2):
+    #     for j in range(i+1,len(nums)-1):
+    #         for k in range(j+1,len(nums)):
+    #             if isPrimeNum(nums[i],nums[j],nums[k]):
+    #                 answer += 1
     comList = list(combinations(nums,3))
     for i in comList:
         if isPrimeNum(i[0],i[1],i[2]):
@@ -11,7 +16,7 @@ def solution(nums):
     return answer
 def isPrimeNum(num1,num2,num3):
     sum = num1+num2+num3
-    for i in range(2,sum):
+    for i in range(2,sum//2+1):
         if sum%i == 0:
             return False
     return True
