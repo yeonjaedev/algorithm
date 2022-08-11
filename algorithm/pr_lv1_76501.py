c3ba7db5e2084feee34eee9dc9775b3b1632db8f
yeonjaedev <yjae47@gmail.com>
2022-08-11 21:25:34 +0900

    [UPDATE] 한줄 풀이 76501

--- algorithm/pr_lv1_76501.py
+++ algorithm/pr_lv1_76501.py
@@ -5,3 +5,8 @@ def solution(absolutes, signs):
     for i in range(len(signs)):
         answer += (1 if signs[i] else -1) * (absolutes[i])
     return answer
+
+
+# 한 줄로 다른 풀이
+def solution(absolutes, signs):
+    return sum(absolute if sign else -absolute for sign,absolute in zip(signs,absolutes))
