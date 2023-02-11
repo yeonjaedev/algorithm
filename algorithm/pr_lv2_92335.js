78e96283baf15dc34ccf8f86ba917f1249ec8322
yeonjaedev <yjae47@gmail.com>
2023-02-11 20:44:22 +0900

    [ADD] 프로그래머스 2단계 92335 k진수에서 소수 개수 구하기

---
+++ algorithm/pr_lv2_92335.js
@@ -0,0 +1,23 @@
+// k진수에서 소수 개수 구하기
+// https://school.programmers.co.kr/learn/courses/30/lessons/92335
+function solution(n, k) {
+    var answer = 0;
+    var resultStr = '' // k진수로 바꾼 값
+    const isPrime = (num) =>{
+        if(num === 1 || num === 0) return false
+        for ( let i = 2 ; i < Math.sqrt(num+1) ; i ++) {
+            if( num % i === 0) return false
+        }
+        return true
+    }
+    for(let i = 0 ; n>0 ; i++ ){
+        resultStr = n%k+resultStr
+        n=Math.floor(n/k)
+    }
+    // resultStr = n.toString(k) -> k진법 구하는 함수
+    resultStr.split('0').forEach(str=>{
+        if(isPrime(str*1)) answer += 1
+
+    })
+    return answer;
+}
